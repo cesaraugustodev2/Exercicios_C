b87808fb5e413d06ccb1e33d870913281662391b
cesaraugustodev2 <csantos3600@gmail.com>
2025-01-24 20:46:13 -0300

    criado pointer.c

---
+++ pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void){
+//* define a local variable a */
+int a = 1;
+
+/* define a pointer variable, and point it to a using the & operator */
+int * pointer_to_a = &a;
+
+printf("The value a is %d\n", a);
+printf("The value of a is also %d\n", *pointer_to_a);
+}
